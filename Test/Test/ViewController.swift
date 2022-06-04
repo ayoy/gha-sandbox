d2ff47e7c52f09e8a37b97db70815384f905f8d1
Dominik Kapusta <dkapusta@duckduckgo.com>
2022-06-04 08:33:21 +0200

    Add test project

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Dominik Kapusta on 04/06/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
